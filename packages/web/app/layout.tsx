642fb68aa1bb293a08ade6e785fe8d7bade0df79
vadhe <aldyvadhe@gmail.com>
2023-08-05 14:02:19 +0800

    chore(devlinks): add tailwind

--- packages/web/app/layout.tsx
+++ packages/web/app/layout.tsx
@@ -13,7 +13,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <p className='text-red-800'>okokok</p>
+        {children}</body>
     </html>
   );
 }
